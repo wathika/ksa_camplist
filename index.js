f8c78fcbcc53dbbf2d0cb693a04cfbe3fa94af28
Viscount <vcwathika@gmail.com>
2020-02-11 18:51:27 +0000

    simple routing for pages

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const url = require('url');
 
 makeServer = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'} );
-  res.write("Hello World!");
+  let path = url.parse( req.url ).pathname;
+  console.log(path);
+  if (path === '/'){
+    res.writeHead(200, {'Content-Type':'text/plain'} );
+    res.write("List of campsites!!");
+  }
+  else if (path === '/add-campsite'){
+    res.writeHead(200, {'Content-Type':'text/plain'} );
+    res.write("Add a camp-site page!!");
+  }
+  else if (path === '/campsite'){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write("Campsite page");
+  }
+  else {
+    res.writeHead(404, {'Content-Type':'text/plain'});
+    res.write("Error page");
+  }
   res.end();
 };
 
